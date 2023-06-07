f3060e071a1d6af5d3500c9137a068f66b9df5ce
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-06-07 17:42:45 +0200

    commented out stuff

--- src/mobs.ts
+++ src/mobs.ts
@@ -1,27 +1,27 @@
-const baseUrl = 'https://mobster-backend-production.up.railway.app';
-import { Member, Mob } from './main';
+// const baseUrl = 'https://mobster-backend-production.up.railway.app';
+// import { Member, Mob } from './main';
 
-/* Create functions*/
-const makeDivElement = document.createElement('div');
-const makeParagraphElement = document.createElement('p');
+// /* Create functions*/
+// const makeDivElement = document.createElement('div');
+// const makeParagraphElement = document.createElement('p');
 
-/* Already in the DOM */
-const htmlAside = document.getElementsByClassName('header__aside');
+// /* Already in the DOM */
+// const htmlAside = document.getElementsByClassName('header__aside');
 
-/* Api call */
+// /* Api call */
 
-const getData = async (id: string) => {
-  const response = await fetch(`${baseUrl}/mobs/${id}`);
-  const data: Mob = await response.json();
-  return data;
-};
+// const getData = async (id: string) => {
+//   const response = await fetch(`${baseUrl}/mobs/${id}`);
+//   const data: Mob = await response.json();
+//   return data;
+// };
 
-const populate = async (id: string) => {
-  const data = await getData(id);
-  const makeDivElement = document.createElement('div');
-  const makeParagraphElement = document.createElement('p');
-  const populatePage = makeDivElement.appendChild(makeParagraphElement);
-  makeParagraphElement.textContent = data.id;
+// const populate = async (id: string) => {
+//   const data = await getData(id);
+//   const makeDivElement = document.createElement('div');
+//   const makeParagraphElement = document.createElement('p');
+//   const populatePage = makeDivElement.appendChild(makeParagraphElement);
+//   makeParagraphElement.textContent = data.id;
   
  
-};
+// };
