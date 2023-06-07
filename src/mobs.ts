2e9be3e7882c33a69dfcdae916e1cd109a8c99f9
Allan Heremi <allanheremidev@gmail.com>
2023-06-07 17:31:39 +0200

    mobs.ts

--- src/mobs.ts
+++ src/mobs.ts
@@ -1,5 +1,5 @@
 const baseUrl = 'https://mobster-backend-production.up.railway.app';
-
+import { Member, Mob } from './main';
 
 /* Create functions*/
 const makeDivElement = document.createElement('div');
@@ -10,9 +10,18 @@ const htmlAside = document.getElementsByClassName('header__aside');
 
 /* Api call */
 
-const getData = async (id) => {
-    const data = await(`${baseUrl}/mobs/${id}`);
-    return data
-}
+const getData = async (id: string) => {
+  const response = await fetch(`${baseUrl}/mobs/${id}`);
+  const data: Mob = await response.json();
+  return data;
+};
 
-
+const populate = async (id: string) => {
+  const data = await getData(id);
+  const makeDivElement = document.createElement('div');
+  const makeParagraphElement = document.createElement('p');
+  const populatePage = makeDivElement.appendChild(makeParagraphElement);
+  makeParagraphElement.textContent = data.id;
+  
+ 
+};
