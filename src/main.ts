ae02b2824b292d5b97f91121288dc83ea2a27cbc
rofun <joufang.w@gmail.com>
2023-06-07 23:02:16 +0800

    add some functions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './style.css';
-const baseUrl = 'https://mobster-backend-production.up.railway.app';
+import "./style.css";
+const baseUrl = "https://mobster-backend-production.up.railway.app";
 type Member = {
   name: string;
   id: string;
@@ -13,16 +13,38 @@ type Mob = {
   members: Member[];
 };
 
+const main = document.getElementById("main") as HTMLElement;
+
 const getMobs = async () => {
   const mobs = await fetch(`${baseUrl}/mobs`);
   const data = mobs.json();
   return data;
 };
 
-document.addEventListener('DOMContentLoaded', async () => {
-  console.log('Loaded page.');
+// add id as meta data in the
+const addMob = () => {
+  const input = document.getElementById("aside__input") as HTMLInputElement;
+  const name = input.value;
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.textContent = name;
+  div.appendChild(p);
+  main.appendChild(p);
+};
+
+// to go into the mob information
+const getMobInfo = async () => {
+  const mob = await fetch("https://mobster-backend-production.up.railway.app");
+  // change the content in main, wait for the innerHTML content.
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
   const mobs = await getMobs();
-  console.log(mobs);
-  const p = document.getElementById('mobs') as HTMLParagraphElement;
+  const p = document.getElementById("mobs") as HTMLParagraphElement;
   p.textContent = mobs.join();
 });
+
+const btn = document.getElementById("aside__button") as HTMLButtonElement;
+btn.onclick = addMob;
+
+export type { Member, Mob };
