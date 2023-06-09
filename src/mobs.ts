a6a3899ca628bd0dcba3f9824756edcda3e55cad
rofun <joufang.w@gmail.com>
2023-06-09 22:02:52 +0800

    front end need to be fixed

--- src/mobs.ts
+++ src/mobs.ts
@@ -1,21 +1,21 @@
-import { getBase } from './main';
-import { Member } from './vite-env';
+import { getBase } from "./main";
+import { Member } from "./vite-env";
 // import { renderMember } from './member';
 
 const toggleElement = (selector: string) => {
   const element = document.querySelector(selector) as HTMLDivElement;
   return {
     show: () => {
-      element.classList.remove('hidden');
+      element.classList.remove("hidden");
     },
     hide: () => {
-      element.classList.add('hidden');
+      element.classList.add("hidden");
     },
   };
 };
 
-const mob = toggleElement('.aside__addmob');
-const member = toggleElement('.aside__addmember');
+const mob = toggleElement(".aside__addmob");
+const member = toggleElement(".aside__addmember");
 
 const makeMember = (member: Member) => {
   const section = `<section class = "member" data-id=${member.id}>
@@ -25,7 +25,7 @@ const makeMember = (member: Member) => {
   return section;
 };
 const makeMembers = function (members: Member[]) {
-  let elements = '';
+  let elements = "";
   members.forEach((member: Member) => {
     elements += makeMember(member);
   });
@@ -35,23 +35,24 @@ const makeMembers = function (members: Member[]) {
 const renderMembers = async (mobId: string) => {
   mob.hide();
   member.show();
-  const main = document.querySelector('.main') as HTMLElement;
-  main.innerHTML = '';
-  const response = await fetch(getBase(['mobs', mobId, 'members']));
+  const main = document.querySelector(".display_area") as HTMLElement;
+  main.innerHTML = "";
+  const response = await fetch(getBase(["mobs", mobId, "members"]));
   const members: Member[] = await response.json();
-  const div = document.createElement('div');
-  div.classList.add('main__members');
+  const div = document.createElement("div");
+  div.classList.add("main__members");
   div.innerHTML = makeMembers(members);
   main.appendChild(div);
+  main.classList.add("display_area_members");
 };
 
 const renderMobName = (name: string, id: string) => {
-  const main = document.querySelector('.main') as HTMLElement;
-  const nameContainer = document.createElement('h1') as HTMLHeadElement;
+  const main = document.querySelector(".display_area") as HTMLElement;
+  const nameContainer = document.createElement("h1") as HTMLHeadElement;
   nameContainer.textContent = name;
-  nameContainer.setAttribute('data-id', id);
-  nameContainer.className = 'display_area__mob-name';
+  nameContainer.setAttribute("data-id", id);
   main.appendChild(nameContainer);
+  nameContainer.classList.add("display_area_members");
 };
 
 export { renderMembers, renderMobName };
