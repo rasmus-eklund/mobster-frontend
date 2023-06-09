8d993fbf9d6dcd6d181643e4afb5b2d5eb594bcd
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-06-09 13:26:29 +0200

    add member function

--- src/member.ts
+++ src/member.ts
@@ -1,6 +1,21 @@
-// import { Member } from './vite-env';
+import { getBase } from './main';
+
 const renderMember = (id: string) => {
   console.log(id);
+};
+
+const addMember = async (
+  body: { name: string; height: number },
+  mobId: string
+) => {
+  const url = getBase(['mobs', mobId, 'members']);
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
 };
 
 export { renderMember };
